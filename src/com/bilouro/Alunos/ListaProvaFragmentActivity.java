60aef3e7acd32d0a4efa11330cae0433a956d056
bilouro <bilouro@bilouro.com>
2013-08-23 01:51:06 -0300

    salvando o estado da activity | saving the activity state

--- src/com/bilouro/Alunos/ListaProvaFragmentActivity.java
+++ src/com/bilouro/Alunos/ListaProvaFragmentActivity.java
@@ -1,6 +1,5 @@
 package com.bilouro.Alunos;
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,24 +8,23 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import com.bilouro.modelo.Aluno;
 import com.bilouro.modelo.Prova;
 
-import java.util.Arrays;
-
 public class ListaProvaFragmentActivity extends Fragment{
+
+    private Prova[] prova_array;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.provas_list_fragment, container, false);
 
-        Prova[] prova_array = new Prova[] {
-                new Prova("16/02/2013", "Matemática", new String[]{"Quanto é 2 + 2?", "Qual a raiz de 9?", "Qual o dobro de 10?", "Fatore 45"} ),
-                new Prova("20/12/2010", "Portugues",  new String[]{"Qual o sujeito da frase?", "Conserto ou Concerto?", "O a é crazeado?", } ),
-                new Prova("26/10/2012", "Inglês", new String[]{"What's your name?", "How are you?", "Loves me loves my dog", } ),
-                new Prova("01/07/2010", "História",  new String[]{"Quem descobriu o Brasil?", "Quando?", "Por que parte do litoral?", "Como se chamou a expedição?" } ),
-        };
+        if (getArguments() != null){
+            prova_array = (Prova[]) getArguments().getSerializable(getResources().getString(R.string.const_prova_lista));
+
+            if ( prova_array == null ) prova_array = new Prova[0];
+        }
+
         ArrayAdapter<Prova> adapter = new ArrayAdapter<Prova>(getActivity(), android.R.layout.simple_list_item_1, prova_array);
 
         ListView lv = (ListView) view.findViewById(R.id.listView);
--- src/com/bilouro/Alunos/ProvaActivity.java
+++ src/com/bilouro/Alunos/ProvaActivity.java
@@ -7,14 +7,18 @@ import com.bilouro.modelo.Prova;
 
 public class ProvaActivity extends FragmentActivity{
     private Prova prova_selecionada;
+    private Prova[] prova_array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.provas);
 
+        restore(savedInstanceState);
+        popula_provas();
+
         FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-        t.replace(R.id.frame_esquerda, new ListaProvaFragmentActivity(), ListaProvaFragmentActivity.class.getCanonicalName());
+        t.replace(R.id.frame_esquerda, getListaProvaFragmentActivity(prova_array), ListaProvaFragmentActivity.class.getCanonicalName());
         if (isTablet()) {
 
             DetalhesProvaFragment detalhesProvaFragment = getDetalhesProvaFragment(prova_selecionada);
@@ -23,12 +27,37 @@ public class ProvaActivity extends FragmentActivity{
         t.commit();
     }
 
+    private void popula_provas() {
+
+        if (prova_array == null) {
+            prova_array = new Prova[] {
+                    new Prova("16/02/2013", "Matemática", new String[]{"Quanto é 2 + 2?", "Qual a raiz de 9?", "Qual o dobro de 10?", "Fatore 45"} ),
+                    new Prova("20/12/2010", "Portugues",  new String[]{"Qual o sujeito da frase?", "Conserto ou Concerto?", "O a é crazeado?", } ),
+                    new Prova("26/10/2012", "Inglês", new String[]{"What's your name?", "How are you?", "Loves me loves my dog", } ),
+                    new Prova("01/07/2010", "História",  new String[]{"Quem descobriu o Brasil?", "Quando?", "Por que parte do litoral?", "Como se chamou a expedição?" } ),
+            };
+        }
+
+        if (this.prova_selecionada == null && prova_array != null && prova_array.length > 0) {
+            this.prova_selecionada = prova_array[0];
+        }
+
+    }
+
+    private ListaProvaFragmentActivity getListaProvaFragmentActivity(Prova[] prova_array) {
+        ListaProvaFragmentActivity listaProvaFragmentActivity = new ListaProvaFragmentActivity();
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(getString(R.string.const_prova_lista), prova_array);
+        listaProvaFragmentActivity.setArguments( bundle );
+        return listaProvaFragmentActivity;
+    }
+
     public void seleciona_prova(Prova prova_selecionada) {
 
         this.prova_selecionada = prova_selecionada;
 
         DetalhesProvaFragment detalhesProvaFragment = getDetalhesProvaFragment(prova_selecionada);
-
         final FragmentTransaction t = getSupportFragmentManager().beginTransaction();
         if (isTablet()) {
             t.replace(R.id.frame_direita, detalhesProvaFragment, DetalhesProvaFragment.class.getCanonicalName());
@@ -56,4 +85,17 @@ public class ProvaActivity extends FragmentActivity{
         return getResources().getBoolean(R.bool.isTablet);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(getResources().getString(R.string.const_prova_selecionada), this.prova_selecionada);
+        outState.putSerializable(getResources().getString(R.string.const_prova_lista), this.prova_array);
+    }
+
+    private void restore(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            this.prova_selecionada = (Prova) savedInstanceState.getSerializable(getResources().getString(R.string.const_prova_selecionada));
+            this.prova_array = (Prova[]) savedInstanceState.getSerializable(getResources().getString(R.string.const_prova_lista));
+        }
+    }
 }
